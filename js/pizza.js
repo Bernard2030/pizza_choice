9c40eb9f8d1583335b9974db86b4f89a9d6cd0a2
Bernard2030 <bernard.mukhenye@student.moringa.com>
2021-08-01 22:00:32 +0300

    initial commit

---
+++ js/pizza.js
@@ -0,0 +1,70 @@
+// $(document).ready(function(){
+//     $("#newForm").submit(function(event){
+
+//         event.preventDefault();
+//         function flavor(){
+//             let pizzaFlavour =document.getElementById("flavor").value;
+//             return parseInt(pizzaFlavour);
+//         }
+//     })
+//     $("#form1").submit(function(event){
+
+//         event.preventDefault();
+//         function topping() {
+//             let pizzaTopping = document.getElementById("topping").value;
+//             return parseInt(pizzaTopping)
+//         }
+//     })
+//     $("#form2").submit(function(event){
+
+//         event.preventDefault();
+//         function crust(){
+//             let pizzaCrust = document.getElementById("crust").value;
+//             return parseInt(pizzaCrust);
+//         }
+//     });
+//     $("#form3").submit(function(event){
+
+//         event.preventDefault();
+//         function size(){
+//             let pizzaSize = document.getElementById("size").value;
+//             return parseInt(pizzaSize);
+//         }
+//     });
+//     function Number(){
+//         let pizzaNumber = document.getElementById("quantity").value;
+//         return parseInt(pizzaNumber);
+//     }
+
+
+//     let flavorName = $("#flavor option: selected").text();
+    
+//     let toppingName = $("#topping option: selected").text();
+
+//     let crustName = $("#crust option: selected").text();
+    
+//     let sizeName = $("#size option: selected").text();
+//     let number = $("#quantity").val();
+
+//     // place order
+
+//    function Order(flavor, topping, crust,size, quantity){
+//        this.newFlavor = flavor;
+//        this.newTopping = topping;
+//        this.newCrust = crust;
+//        this.newSize = size;
+//        this.newQuantity = quantity;
+//    }
+
+//    let userInput = new Order(flavor(), topping(), crust(), size(), quantity());
+//    let totalCost =
+//    userInput.newSize +
+//    userInput.newCrust +
+//    userInput.newTopping +
+//    userInput.newFlavor *
+//    userInput.newQuantity;
+
+//    if(confirm('would you like delivery?')){
+//        prompt("enter location");
+//    }
+// })
